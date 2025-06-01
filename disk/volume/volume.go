4f7222bb1545c6d2197f0d8e9aae118576055629
Armen Arsakian <aarsakian@gmail.com>
2025-06-01 12:57:59 +0300

    interface changes

--- disk/volume/volume.go
+++ disk/volume/volume.go
@@ -1,13 +1,16 @@
 package volume
 
-import "github.com/aarsakian/FileSystemForensics/img"
+import (
+	"github.com/aarsakian/FileSystemForensics/FS/NTFS/MFT"
+	"github.com/aarsakian/FileSystemForensics/img"
+)
 
 type Volume interface {
 	Process(img.DiskReader, int64, []int, int, int)
 	GetSectorsPerCluster() int
 	GetBytesPerSector() uint64
 	GetInfo() string
-	//GetFSMetadata() []MFT.Record
+	GetFS() []MFT.Record
 	CollectUnallocated(img.DiskReader, int64, chan<- []byte)
 	GetSignature() string
 }
