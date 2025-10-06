4437b9a74756630292ccdb71429df4937413abd2
Armen Arsakian <aarsakian@gmail.com>
2025-10-06 21:51:57 +0300

    data types flags and relevant functions

--- FS/NTFS/MFT/attributes/data.go
+++ FS/NTFS/MFT/attributes/data.go
@@ -2,6 +2,13 @@ package attributes
 
 import "fmt"
 
+var dataFlags = map[uint16]string{
+	0x0000: "None",
+	0x0001: "Compressed",
+	0x4000: "Encrypted",
+	0x8000: "Encrypted",
+}
+
 type DATA struct {
 	Content []byte
 	Header  *AttributeHeader
@@ -27,5 +34,15 @@ func (data DATA) IsNoNResident() bool {
 }
 
 func (data DATA) ShowInfo() {
-	fmt.Printf("type %s %t \n", data.FindType(), data.IsNoNResident())
+	fmt.Printf("type %s %t %s \n", data.FindType(), data.IsNoNResident(), data.GetType())
+}
+
+func (data DATA) GetType() string {
+	var dataType string
+	for key, value := range dataFlags {
+		if key&data.Header.Flags != 0 {
+			dataType += " " + value
+		}
+	}
+	return dataType
 }
