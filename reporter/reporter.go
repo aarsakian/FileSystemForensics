76f3f69b6fba2b4cb38833da64d5c453fde96a93
Armen Arsakian <aarsakian@gmail.com>
2026-03-15 12:36:29 +0200

    functionality to print clusters for respective record allocated clusters in shadow volumes

--- reporter/reporter.go
+++ reporter/reporter.go
@@ -1,7 +1,10 @@
 package reporter
 
 import (
+	"fmt"
+
 	metadata "github.com/aarsakian/FileSystemForensics/FS"
+	VssLib "github.com/aarsakian/FileSystemForensics/FS/NTFS/VSS"
 	UsnJrnl "github.com/aarsakian/FileSystemForensics/FS/NTFS/usnjrnl"
 	"github.com/aarsakian/FileSystemForensics/tree"
 )
@@ -25,7 +28,8 @@ type Reporter struct {
 	ShowClusters    bool
 }
 
-func (rp Reporter) Show(records []metadata.Record, usnjrnlRecords UsnJrnl.Records, partitionId int, tree tree.Tree) {
+func (rp Reporter) Show(records []metadata.Record, usnjrnlRecords UsnJrnl.Records, partitionId int,
+	tree tree.Tree, shadowVol *VssLib.ShadowVolume) {
 	for _, record := range records {
 
 		if record.GetID() == 0 {
@@ -34,12 +38,10 @@ func (rp Reporter) Show(records []metadata.Record, usnjrnlRecords UsnJrnl.Record
 
 		if rp.ShowFileName != "" || rp.ShowFull {
 			record.ShowAttributes("FileName")
-
 		}
 
 		if rp.ShowAttributes != "" || rp.ShowFull {
 			record.ShowAttributes(rp.ShowAttributes)
-
 		}
 
 		if rp.ShowTimestamps || rp.ShowFull {
@@ -49,28 +51,22 @@ func (rp Reporter) Show(records []metadata.Record, usnjrnlRecords UsnJrnl.Record
 
 		if rp.IsResident || rp.ShowFull {
 			record.ShowIsResident()
-
 		}
 
 		if rp.ShowRunList || rp.ShowFull {
 			record.ShowRunList()
-
 		}
 
 		if rp.ShowFileSize || rp.ShowFull {
 			record.ShowFileSize()
-
 		}
 
 		if rp.ShowVCNs || rp.ShowFull {
 			record.ShowVCNs()
-
 		}
 
 		if rp.ShowIndex || rp.ShowFull {
-
 			record.ShowIndex()
-
 		}
 
 		if rp.ShowParent || rp.ShowFull {
@@ -88,6 +84,19 @@ func (rp Reporter) Show(records []metadata.Record, usnjrnlRecords UsnJrnl.Record
 		if rp.ShowReparse || rp.ShowFull {
 			record.ShowAttributes("Reparse Point")
 		}
+
+		if rp.ShowVSSClusters || rp.ShowFull {
+			clusters := record.GetAllocatedClusters()
+
+			for idx, offset := range shadowVol.GetClustersInfo(4096, clusters) {
+				if offset == -1 {
+					fmt.Printf("%d cl shadow offset not found\n", clusters[idx])
+				} else {
+					fmt.Printf("%d cl shadow offset cl %d\n", clusters[idx], offset)
+				}
+			}
+		}
+
 	}
 
 	for _, record := range usnjrnlRecords {
